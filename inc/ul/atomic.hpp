ded882a8cdfb9951edff4e9fd68e9fd9a38df741
Bruno Santos <bsantos@cppdev.net>
2013-06-27 22:44:08 +0100

    Refactor atomic

--- inc/ul/atomic.hpp
+++ inc/ul/atomic.hpp
@@ -20,13 +20,123 @@
 ///////////////////////////////////////////////////////////////////////////////
 namespace ul {
 
+///////////////////////////////////////////////////////////////////////////////
+namespace detail {
+
+///////////////////////////////////////////////////////////////////////////
+#ifdef __GNUC__
+inline void atomic_barrier()
+{
+	__sync_synchronize();
+}
+
+template<class T>
+inline T atomic_inc_and_fetch(T volatile* dst)
+{
+	return __sync_add_and_fetch(dst, 1);
+}
+
+template<class T>
+inline T atomic_dec_and_fetch(T volatile* dst)
+{
+	return __sync_sub_and_fetch(dst, 1);
+}
+
+template<class T>
+inline T atomic_add_and_fetch(T volatile* dst, T val)
+{
+	return __sync_add_and_fetch(dst, val);
+}
+
+template<class T>
+inline T atomic_sub_and_fetch(T volatile* dst, T val)
+{
+	return __sync_sub_and_fetch(dst, val);
+}
+
+template<class T>
+inline T atomic_or_and_fetch(T volatile* dst, T val)
+{
+	return __sync_or_and_fetch(dst, val);
+}
+
+template<class T>
+inline T atomic_and_and_fetch(T volatile* dst, T val)
+{
+	return __sync_and_and_fetch(dst, val);
+}
+
+template<class T>
+inline T atomic_xor_and_fetch(T volatile* dst, T val)
+{
+	return __sync_xor_and_fetch(dst, val);
+}
+
+template<class T>
+inline T atomic_fetch_and_inc(T volatile* dst)
+{
+	return __sync_fetch_and_add(dst, 1);
+}
+
+template<class T>
+inline T atomic_fetch_and_dec(T volatile* dst)
+{
+	return __sync_fetch_and_sub(dst, 1);
+}
+
+template<class T>
+inline T atomic_fetch_and_add(T volatile* dst, T val)
+{
+	return __sync_fetch_and_add(dst, val);
+}
+
+template<class T>
+inline T atomic_fetch_and_sub(T volatile* dst, T val)
+{
+	return __sync_fetch_and_sub(dst, val);
+}
+
+template<class T>
+inline T atomic_fetch_and_or(T volatile* dst, T val)
+{
+	return __sync_fetch_and_or(dst, val);
+}
+
+template<class T>
+inline T atomic_fetch_and_and(T volatile* dst, T val)
+{
+	return __sync_fetch_and_and(dst, val);
+}
+
+template<class T>
+inline T atomic_fetch_and_xor(T volatile* dst, T val)
+{
+	return __sync_fetch_and_xor(dst, val);
+}
+
+template<class T>
+inline T atomic_swap(T volatile* dst, T val)
+{
+	return __sync_lock_test_and_set(dst, val);
+}
+
+template<class T>
+inline T atomic_compare_and_swap(T volatile* dst, T compare, T val)
+{
+	return __sync_val_compare_and_swap(dst, compare, val);
+}
+
+#else
+#	error unsupported compiler
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+} /* namespace detail */
+
 ///////////////////////////////////////////////////////////////////////////
 template<class T>
 class atomic {
-	struct safe_bool_t {
-		void true_() {}
-	};
-	typedef void (safe_bool_t::*safe_bool)();
+	UL_UNDEFINED_BOOL;
 
 public:
 	T fetch_and_add(T val) { return detail::atomic_fetch_and_add(&_value, val); }
@@ -52,7 +162,12 @@ public:
 	T operator++(int) { return detail::atomic_fetch_and_inc(&_value); }
 	T operator--(int) { return detail::atomic_fetch_and_dec(&_value); }
 
-	operator safe_bool()                  { return _value ? &safe_bool_t::true_ : 0; }
+	operator undefined_bool()
+	{
+		return _value ? UL_UNDEFINED_BOOL_TRUE
+		              : UL_UNDEFINED_BOOL_FALSE;
+	}
+
 	bool operator!() const                { return !_value; }
 	bool operator!=(volatile T val) const { return (_value != val); }
 	bool operator==(volatile T val) const { return (_value == val); }
